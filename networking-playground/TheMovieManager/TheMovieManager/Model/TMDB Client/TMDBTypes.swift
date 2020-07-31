1e273000bdbd3fd079bc84cc238cbafa388496bd
Jon DeMaagd <jdemaagd7@outlook.com>
2020-07-31 19:52:24 -0400

    cleanup

--- networking-playground/TheMovieManager/TheMovieManager/Model/TMDB Client/TMDBTypes.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-enum MediaType: String, Codable {
-    case movie, tv
-}
--- networking-playground/TheMovieManager/TheMovieManager/View/Colors.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-extension UIColor {
-    static let primaryDark = UIColor(named: "PrimaryDark")!
-    static let primaryLight = UIColor(named: "PrimaryLight")!
-    static let gradientTop = UIColor(named: "GradientTop")!
-    static let gradientBottom = UIColor(named: "GradientBottom")!
-}
